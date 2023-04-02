8f76a965a77834c39ecdc2851bf21548d5790857
WhyK <windchimeyk@gmail.com>
2023-04-03 02:06:13 +0900

    style: deno lint

--- core/ps.ts
+++ core/ps.ts
@@ -27,7 +27,7 @@ export const fetchMovieInfo = async (
     );
 
     return result;
-  } catch (error) {
+  } catch (_error) {
     return [];
   }
 };
@@ -64,7 +64,7 @@ export const fetchTheaterInfo = async (
     );
 
     return result;
-  } catch (error) {
+  } catch (_error) {
     return [];
   }
 };
