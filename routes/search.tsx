e4238b0078a973474fa8700c5ae1247cbe353f38
WhyK <windchimeyk@gmail.com>
2023-04-09 13:35:24 +0900

    refactor: replace search to Fresh

--- routes/search.tsx
+++ routes/search.tsx
@@ -9,12 +9,19 @@ import type { MovieInfo } from "../model.ts";
 
 type HandlerProps = {
   req: Request;
-  search: string | null;
+  search?: string | null;
   movies: Array<MovieInfo>;
 };
 
 export const handler: Handlers<HandlerProps> = {
   async GET(req, ctx) {
+    const movies = await fetchMovieInfo({
+      table: "tbl_movieinfo",
+      fields: ["title", "view_date", "view_start_time", "view_end_time"],
+    });
+    return ctx.render({ req, movies });
+  },
+  async POST(req, ctx) {
     const body = await getUrlParams(req);
     const search = body.get("search");
     const movies = await fetchMovieInfo({
@@ -24,7 +31,7 @@ export const handler: Handlers<HandlerProps> = {
       like: search,
     });
     return ctx.render({ req, movies, search });
-  },
+  }
 };
 
 const PAGE_TITLE = "鑑賞作品の検索";
