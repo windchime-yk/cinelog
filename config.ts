6ec04ce1daabfab49c9de1b9c5a7dcf979321d10
WhyK <windchimeyk@gmail.com>
2023-08-05 09:33:56 +0900

    style: deno fmt

--- config.ts
+++ config.ts
@@ -5,8 +5,12 @@ export const config = {
   username: Deno.env.get("USERNAME"),
   password: Deno.env.get("PASSWORD"),
   ps_host: Deno.env.get("PS_HOST"),
-  ps_username: Deno.env.get("DEVELOP") ? Deno.env.get("PS_DEV_USERNAME") : Deno.env.get("PS_USERNAME"),
-  ps_password: Deno.env.get("DEVELOP") ? Deno.env.get("PS_DEV_PASSWORD") : Deno.env.get("PS_PASSWORD"),
+  ps_username: Deno.env.get("DEVELOP")
+    ? Deno.env.get("PS_DEV_USERNAME")
+    : Deno.env.get("PS_USERNAME"),
+  ps_password: Deno.env.get("DEVELOP")
+    ? Deno.env.get("PS_DEV_PASSWORD")
+    : Deno.env.get("PS_PASSWORD"),
 };
 
 export const SITE_NAME = "シネログ";
