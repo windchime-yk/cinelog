efa56acecf8ea2d90e7e0acd12a1812388e4a0ca
WhyK <windchimeyk@gmail.com>
2023-08-05 09:28:53 +0900

    refactor: adapt develop DB

--- config.ts
+++ config.ts
@@ -1,14 +1,12 @@
-import { load } from "std/dotenv/mod.ts";
-
-const { USERNAME, PASSWORD, PS_HOST, PS_USERNAME, PS_PASSWORD } = await load();
+import "std/dotenv/load.ts";
 
 /** 環境変数 */
 export const config = {
-  username: USERNAME ?? Deno.env.get("USERNAME"),
-  password: PASSWORD ?? Deno.env.get("PASSWORD"),
-  ps_host: PS_HOST ?? Deno.env.get("PS_HOST"),
-  ps_username: PS_USERNAME ?? Deno.env.get("PS_USERNAME"),
-  ps_password: PS_PASSWORD ?? Deno.env.get("PS_PASSWORD"),
+  username: Deno.env.get("USERNAME"),
+  password: Deno.env.get("PASSWORD"),
+  ps_host: Deno.env.get("PS_HOST"),
+  ps_username: Deno.env.get("DEVELOP") ? Deno.env.get("PS_DEV_USERNAME") : Deno.env.get("PS_USERNAME"),
+  ps_password: Deno.env.get("DEVELOP") ? Deno.env.get("PS_DEV_PASSWORD") : Deno.env.get("PS_PASSWORD"),
 };
 
 export const SITE_NAME = "シネログ";
