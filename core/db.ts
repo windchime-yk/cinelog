258388a00aee079dec847ba24a718082ed1d201f
WhyK <windchimeyk@gmail.com>
2023-11-26 16:25:30 +0900

    refactor: add getCardData

--- core/db.ts
+++ core/db.ts
@@ -1,5 +1,8 @@
-import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { desc, sql } from "drizzle-orm";
+import { drizzle  } from "drizzle-orm/planetscale-serverless";
 import { connect } from "planetscale";
+import { movieTable } from "~/db/schema.ts";
+import type { PickMovie } from "~/db/model.ts";
 import "$std/dotenv/load.ts";
 
 const connection = connect({
@@ -13,3 +16,38 @@ const connection = connect({
 });
 
 export const db = drizzle(connection);
+
+/**
+ * Cardコンポーネントの表示に必要なデータをDBから取得
+ * @param limit 取得件数
+ */
+export const getCardData = async (limit?: number): Promise<Array<PickMovie>> => {
+  let movies: Array<PickMovie>
+  try {
+    if (!limit) {
+      movies = await db.select({
+        title: movieTable.title,
+        view_date: sql<
+          string
+        >`DATE_FORMAT(DATE(${movieTable.view_start_datetime}), '%Y/%m/%d')`,
+        diff: sql<
+          string
+        >`TIMESTAMPDIFF(MINUTE, ${movieTable.view_start_datetime}, ${movieTable.view_end_datetime})`,
+      }).from(movieTable).orderBy(desc(movieTable.view_start_datetime));
+    } else {
+      movies = await db.select({
+        title: movieTable.title,
+        view_date: sql<
+          string
+        >`DATE_FORMAT(DATE(${movieTable.view_start_datetime}), '%Y/%m/%d')`,
+        diff: sql<
+          string
+        >`TIMESTAMPDIFF(MINUTE, ${movieTable.view_start_datetime}, ${movieTable.view_end_datetime})`,
+      }).from(movieTable).limit(limit).orderBy(desc(movieTable.view_start_datetime));
+    }
+  } catch (_error) {
+    movies = []
+  }
+
+  return movies
+}
