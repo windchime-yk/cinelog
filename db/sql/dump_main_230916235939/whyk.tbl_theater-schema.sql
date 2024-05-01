396162133ba9ee209278a0f504164ed6d9056f19
WhyK <windchimeyk@gmail.com>
2024-05-02 00:43:10 +0900

    chore(db): delete old schema

--- db/sql/dump_main_230916235939/whyk.tbl_theater-schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tbl_theater` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(246) NOT NULL COMMENT '上映館',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- db/sql/dump_main_240308155815/whyk.tbl_theater-schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `tbl_theater` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(246) NOT NULL COMMENT '上映館',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  UNIQUE KEY `name` (`name`)
-) AUTO_ID_CACHE=1;
