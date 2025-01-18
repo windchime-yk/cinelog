e763f1dd9ee157db056f5959b77ff7660a50452c
WhyK <windchimeyk@gmail.com>
2025-01-19 01:25:31 +0900

    style: deno fmt

--- scripts/db-dump.ts
+++ scripts/db-dump.ts
@@ -1,4 +1,7 @@
-const DATABASE_BRANCH = Deno.env.get("DATABASE_BRANCH") as "main" | "develop" | undefined;
+const DATABASE_BRANCH = Deno.env.get("DATABASE_BRANCH") as
+  | "main"
+  | "develop"
+  | undefined;
 const datetimeFormatter = new Intl.DateTimeFormat("ja-JP", {
   year: "2-digit",
   month: "2-digit",
@@ -9,14 +12,20 @@ const datetimeFormatter = new Intl.DateTimeFormat("ja-JP", {
   timeZone: "Asia/Tokyo",
 });
 const currentDatetime = datetimeFormatter.format(new Date()).replace(/\D/g, "");
-const username = DATABASE_BRANCH === "main" ? Deno.env.get("DB_USERNAME") : Deno.env.get("DB_DEV_USERNAME")
-const password = DATABASE_BRANCH === "main" ? Deno.env.get("DB_PASSWORD") : Deno.env.get("DB_DEV_PASSWORD")
+const username = DATABASE_BRANCH === "main"
+  ? Deno.env.get("DB_USERNAME")
+  : Deno.env.get("DB_DEV_USERNAME");
+const password = DATABASE_BRANCH === "main"
+  ? Deno.env.get("DB_PASSWORD")
+  : Deno.env.get("DB_DEV_PASSWORD");
 
 // tiup dumpling -u <ユーザー名> -P 4000 -h <TiDB Serverlessのエンドポイント> --filetype sql -t 8 -o /tmp/test -r 200000 -F 4MiB -p <パスワード>
 // https://dev.classmethod.jp/articles/export-tidb-data-with-dumpling/
 const command = new Deno.Command("tiup", {
-  args:
-    `dumpling -u ${username} -P 4000 -h ${Deno.env.get("DB_HOST")} -B cinelog --filetype sql -t 8 -o db/sql/dump_${DATABASE_BRANCH}_${currentDatetime} -p ${password}`.split(" ")
+  args: `dumpling -u ${username} -P 4000 -h ${
+    Deno.env.get("DB_HOST")
+  } -B cinelog --filetype sql -t 8 -o db/sql/dump_${DATABASE_BRANCH}_${currentDatetime} -p ${password}`
+    .split(" "),
 });
 const { success, stdout, stderr } = await command.output();
 const decoder = new TextDecoder();
